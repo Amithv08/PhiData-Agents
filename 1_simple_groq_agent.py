11389f8c90b87c69b755f4791e59d583e9fbcecd
Amith Yadav <amithcoolv@gmail.com>
2025-02-05 14:35:15 -0600

    Added few comments about env

--- 1_simple_groq_agent.py
+++ 1_simple_groq_agent.py
@@ -3,7 +3,7 @@ from phi.model.groq import Groq
 from dotenv import load_dotenv
 
 
-load_dotenv()
+load_dotenv()     # I have deleted the env file, as code is on public, add OPENAPIKEY AND GROQ API KEY on local.
 
 agent = Agent(
     model = Groq(id="llama-3.3-70b-versatile")
